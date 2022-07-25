9ebc3366282be68826a3afac218befca2546b8a5
vitorfail <95591747+vitorfail@users.noreply.github.com>
2022-07-25 11:20:30 -0300

    $

--- src/componentes/BlocosCaixa/BlocosCaixa.js
+++ src/componentes/BlocosCaixa/BlocosCaixa.js
@@ -6,11 +6,11 @@ function BlocosCaixa(){
         <div className="blocos-caixa">
             <div className="box">
                 <h2 className="receita">R$ 3.456.908,00</h2>
-                <h1 className="receita">Recebido Hoje</h1>
+                <h1 >Recebido Hoje</h1>
             </div>
             <div className="box">
                 <h2 className="despesas">R$ 970,089</h2>
-                <h1 className="despesas">Despesas</h1>
+                <h1 >Despesas</h1>
             </div>
             <div className="box-hora">
                 <h2 className="titulo">12:00</h2>
--- src/componentes/CaixaMostra/CaixaMostra.js
+++ src/componentes/CaixaMostra/CaixaMostra.js
@@ -3,7 +3,13 @@ import "./CaixaMostra.css";
 function CaixaMostra(){
     return(
         <div className="mostrar-caixa">
-
+            <div className="tabela">
+                <div className="titulo">
+                    <h3>Descrição</h3>
+                    <h3>Valores</h3>
+                </div>
+                
+            </div>
         </div>
     )
 }
