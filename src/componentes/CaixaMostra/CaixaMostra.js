f1838291e86303d08eb75144bd570a4926c7ff4a
vitorfail <vitor_andrademanoel@hotmail.com>
2022-07-25 13:28:46 -0300

    #

--- src/componentes/CaixaMostra/CaixaMostra.js
+++ src/componentes/CaixaMostra/CaixaMostra.js
@@ -5,10 +5,26 @@ function CaixaMostra(){
         <div className="mostrar-caixa">
             <div className="tabela">
                 <div className="titulo">
-                    <h3>Descrição</h3>
+                    <h3 className="descricao">Descrição</h3>
                     <h3>Valores</h3>
                 </div>
+                <div className="entrada">
+                    <div className="descri">
+                        <h3>Roupa</h3>
+                    </div>
+                    <div className="valor">
+                        <h3>R$ 43,00</h3>
+                    </div>
+                </div>
+                <div className="saida">
+                    <div className="descri">
+                        <h3>Papel</h3>
+                    </div>
+                    <div className="valor">
+                        <h3>- R$ 13,00</h3>
+                    </div>
                 
+                </div>
             </div>
         </div>
     )
