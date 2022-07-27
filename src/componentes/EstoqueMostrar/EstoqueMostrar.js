3b745080adf0bc68fa24535eb5154ce89e7be50f
vitorfail <95591747+vitorfail@users.noreply.github.com>
2022-07-27 15:07:25 -0300

    GG

--- src/componentes/EstoqueMostrar/EstoqueMostrar.js
+++ src/componentes/EstoqueMostrar/EstoqueMostrar.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { useState } from "react";
 import "./EstoqueMostrar.css";
+import Editar from "../../icons/editar.png";
+import Excluir from "../../icons/excluir.png";
 
 function EstoqueMostrar(){
     return(
@@ -22,22 +24,11 @@ function EstoqueMostrar(){
                     <div className="preco">
                         <h3>Preço</h3>
                     </div>
-                </div>
-                <div className="entrada">
-                    <div className="descri-2">
-                        <h3>Roupa</h3>
+                    <div className="editar">
+                        <h3>Editar</h3>
                     </div>
-                    <div className="qtd-2">
-                        <h3>300</h3>
-                    </div>
-                    <div className="custo-2">
-                        <h3>300</h3>
-                    </div>
-                    <div className="lucro-2">
-                        <h3>13%</h3>
-                    </div>
-                    <div className="preco-2">
-                        <h3>R$ 43,00</h3>
+                    <div className="excluir">
+                        <h3>Excluir</h3>
                     </div>
                 </div>
                 <div className="entrada">
@@ -55,6 +46,35 @@ function EstoqueMostrar(){
                     </div>
                     <div className="preco-2">
                         <h3>R$ 43,00</h3>
+                    </div>
+                    <div className="editar-2">
+                        <button><img src={Editar}/></button>
+                    </div>
+                    <div className="excluir-2">
+                        <button><img src={Excluir}/></button>
+                    </div>
+                </div>
+                <div className="entrada">
+                    <div className="descri-2">
+                        <h3>Roupa</h3>
+                    </div>
+                    <div className="qtd-2">
+                        <h3>300</h3>
+                    </div>
+                    <div className="custo-2">
+                        <h3>300</h3>
+                    </div>
+                    <div className="lucro-2">
+                        <h3>13%</h3>
+                    </div>
+                    <div className="preco-2">
+                        <h3>R$ 43,00</h3>
+                    </div>
+                    <div className="editar-2">
+                        <button><img src={Editar}/></button>
+                    </div>
+                    <div className="excluir-2">
+                        <button><img src={Excluir}/></button>
                     </div>
                 </div>
             </div>
