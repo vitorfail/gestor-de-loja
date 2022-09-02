14d0a630ab11edf52da0046aeb9536a9b943d29e
vitorfail <vitor_andrademanoel@hotmail.com>
2022-09-02 18:29:42 -0300

    123

--- src/componentes/Faturamento/Faturamento.js
+++ src/componentes/Faturamento/Faturamento.js
@@ -83,7 +83,7 @@ function Faturamento(props){
     }
     return(
         <div className="grafico_faturamento">
-            <div style={{"width":"80%",  "margin-left":"34px"}}>
+            <div style={{"width":"80%",  "marginLeft":"34px"}}>
                 <select className="anos" value={ano_faturamento} onChange={(event) => faturamento_ano(event.target.value)}>
                     <option value="Todos os anos">Todos</option>
                     <option value="1984">1984</option>
@@ -144,7 +144,7 @@ function Faturamento(props){
                     <option value="2040">2040</option>
                 </select>
                 <div className="planilha">
-                <Bar
+                    <Bar
                     data={{labels: ["Janeiro", "Fevereiro", "Março", "Abril", 
                         "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
                     datasets: [{
@@ -265,6 +265,95 @@ function Faturamento(props){
                                         'rgb(201, 203, 207)'
                                         ]}]
                                     }}/>   
+                </div>
+            </div>
+            <div>
+                <div style={{"display":"block"}}>
+                    <div style={{"display":"flex", "justifyContent":"center"}}>
+                        <select className="datas">
+                            <option value='01'>Janeiro</option>
+                            <option value='02'>Fevereiro</option>
+                            <option value='03'>Março</option>
+                            <option value='04'>Abril</option>
+                            <option value='05'>Maio</option>
+                            <option value='06'>Junho</option>
+                            <option value='07'>Julho</option>
+                            <option value='08'>Agosto</option>
+                            <option value='09'>Setembro</option>
+                            <option value='10'>Outubro</option>
+                            <option value='11'>Novembro</option>
+                            <option value='12'>Dezembro</option>
+                        </select>
+                        <select className="datas">
+                            <option value="Todos os anos">Todos</option>
+                            <option value="1984">1984</option>
+                            <option value="1985">1985</option>
+                            <option value="1986">1986</option>
+                            <option value="1987">1987</option>
+                            <option value="1988">1988</option>
+                            <option value="1989">1989</option>
+                            <option value="1990">1990</option>
+                            <option value="1991">1991</option>
+                            <option value="1992">1992</option>
+                            <option value="1993">1993</option>
+                            <option value="1994">1994</option>
+                            <option value="1995">1995</option>
+                            <option value="1996">1996</option>
+                            <option value="1997">1997</option>
+                            <option value="1998">1998</option>
+                            <option value="1999">1999</option>
+                            <option value="2001">2001</option>
+                            <option value="2002">2002</option>
+                            <option value="2003">2003</option>
+                            <option value="2004">2004</option>
+                            <option value="2005">2005</option>
+                            <option value="2006">2006</option>
+                            <option value="2007">2007</option>
+                            <option value="2008">2008</option>
+                            <option value="2009">2009</option>
+                            <option value="2010">2010</option>
+                            <option value="2011">2011</option>
+                            <option value="2012">2012</option>
+                            <option value="2013">2013</option>
+                            <option value="2014">2014</option>
+                            <option value="2015">2015</option>
+                            <option value="2016">2016</option>
+                            <option value="2017">2017</option>
+                            <option value="2018">2018</option>
+                            <option value="2019">2019</option>
+                            <option value="2020">2020</option>
+                            <option value="2021">2021</option>
+                            <option value="2022">2022</option>
+                            <option value="2023">2023</option>
+                            <option value="2024">2024</option>
+                            <option value="2025">2025</option>
+                            <option value="2026">2026</option>
+                            <option value="2027">2027</option>
+                            <option value="2028">2028</option>
+                            <option value="2029">2029</option>
+                            <option value="2030">2030</option>
+                            <option value="2031">2031</option>
+                            <option value="2032">2032</option>
+                            <option value="2033">2033</option>
+                            <option value="2034">2034</option>
+                            <option value="2035">2035</option>
+                            <option value="2036">2036</option>
+                            <option value="2037">2037</option>
+                            <option value="2038">2038</option>
+                            <option value="2039">2039</option>
+                            <option value="2040">2040</option>
+                        </select>
+                    </div>
+                    <div style={{"display": "flex", "justifyContent":"center"}}>
+                        <div className="conta">
+                            <h3 style={{"margin":"0"}}>Conta de luz</h3>
+                            <h3 className="situacao" style={{"margin":"0"}}>Pago</h3>
+                            <label className="switch" >
+                                <input type="checkbox"></input>
+                                <span className="slider round"></span>
+                            </label>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
