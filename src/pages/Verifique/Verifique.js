0fb593759ff6a6923a49162fd3751785833f8e9e
vitorfail <vitor_andrademanoel@hotmail.com>
2022-09-05 18:36:34 -0300

    12wsd

--- src/pages/Verifique/Verifique.js
+++ src/pages/Verifique/Verifique.js
@@ -63,14 +63,70 @@ function Verifique(){
     function encaminhar(){
         history.push('/')
     }
-    function numero_pas(event){
-        if(event.key === 0 || event.key === 1 || event.key === 2 || event.key === 3 
-        || event.key === 4 || event.key === 5 || event.key === 6 || event.key === 7 || event.key === 8 || event.key === 9){
+
+    function um_(event){
+        if(event.target === '0' || event.key === '1' || event.key === '2' || event.key === '3' 
+            || event.key === '4' || event.key === '5' || event.key === '6' || event.key === '7' 
+            || event.key === '8' || event.key === '9'){
+            setum(event.key)
             const form = event.target.form;
             const index = [...form].indexOf(event.target);
             form.elements[index + 1].focus();
             event.preventDefault();
         }
+    }
+    function dois_(event){
+        if(event.key === '0' || event.key === '1' || event.key === '2' || event.key === '3' 
+            || event.key === '4' || event.key === '5' || event.key === '6' || event.key === '7' 
+            || event.key === '8' || event.key === '9'){
+            setdois(event.key)
+            const form = event.target.form;
+            const index = [...form].indexOf(event.target);
+            form.elements[index + 1].focus();
+            event.preventDefault();
+        }
+    }
+    function tres_(event){
+        if(event.key === '0' || event.key === '1' || event.key === '2' || event.key === '3' 
+            || event.key === '4' || event.key === '5' || event.key === '6' || event.key === '7' 
+            || event.key === '8' || event.key === '9'){
+            settres(event.key)
+            const form = event.target.form;
+            const index = [...form].indexOf(event.target);
+            form.elements[index + 1].focus();
+            event.preventDefault();
+        }
+    }
+    function quatro_(event){
+        if(event.key === '0' || event.key === '1' || event.key === '2' || event.key === '3' 
+            || event.key === '4' || event.key === '5' || event.key === '6' || event.key === '7' 
+            || event.key === '8' || event.key === '9'){
+            setquatro(event.key)
+            const form = event.target.form;
+            const index = [...form].indexOf(event.target);
+            form.elements[index + 1].focus();
+            event.preventDefault();
+        }
+    }
+    function cinco_(event){
+        if(event.key === '0' || event.key === '1' || event.key === '2' || event.key === '3' 
+            || event.key === '4' || event.key === '5' || event.key === '6' || event.key === '7' 
+            || event.key === '8' || event.key === '9'){
+            setcinco(event.key)
+            const form = event.target.form;
+            const index = [...form].indexOf(event.target);
+            form.elements[index + 1].focus();
+            event.preventDefault();
+        }
+    }
+    function seis_(event){
+        if(event.key === '0' || event.key === '1' || event.key === '2' || event.key === '3' 
+            || event.key === '4' || event.key === '5' || event.key === '6' || event.key === '7' 
+            || event.key === '8' || event.key === '9'){
+            setseis(event.key)
+        }
+    }
+    function vazio(){
     }
     return(
     <div className="verifique-back">
@@ -97,15 +153,15 @@ function Verifique(){
             <h3 className={aviso_net}>Verifique sua internet, ou fale com o fornecedor*</h3>
             <h3>Digite o codigo de verificação que chegou em seu email</h3>
             <form className="entradas">
-                <input maxLength={1} onKeyDown={(event) => numero_pas(event)}></input>
-                <input maxLength={1} onKeyDown={(event) => numero_pas(event)}></input>
-                <input maxLength={1} onKeyDown={(event) => numero_pas(event)}></input>
-                <input maxLength={1} onKeyDown={(event) => numero_pas(event)}></input>
-                <input maxLength={1} onKeyDown={(event) => numero_pas(event)}></input>
-                <input maxLength={1} onKeyDown={(event) => numero_pas(event)}></input>
+                <input maxLength={1} value={um}  onChange={() => vazio()} onKeyDown={(event) => um_(event)}></input>
+                <input maxLength={1} value={dois}  onChange={() => vazio()} onKeyDown={(event) => dois_(event)}></input>
+                <input maxLength={1} value={tres}  onChange={() => vazio()} onKeyDown={(event) => tres_(event)}></input>
+                <input maxLength={1} value={quatro}  onChange={() => vazio()} onKeyDown={(event) => quatro_(event)}></input>
+                <input maxLength={1} value={cinco}  onChange={() => vazio()} onKeyDown={(event) => cinco_(event)}></input>
+                <input maxLength={1} value={seis}  onChange={() =>vazio()} onKeyDown={(event) => seis_(event)}></input>
             </form>
             <div className="botoes">
-                <button onClick={(e) => login()}>Entrar</button>
+                <button onClick={(e) => login()}>Verificar</button>
             </div>
         </div>
     </div>
